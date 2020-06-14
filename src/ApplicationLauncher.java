176d0eaec31873e162df008cc67d505272f08d51
Erik-Z <erikzhou7@gmail.com>
2020-06-14 13:19:05 -0400

    Implemented Base Code for JavaFX

--- src/ApplicationLauncher.java
+++ src/ApplicationLauncher.java
@@ -1,2 +1,8 @@
-public class ApplicationLauncher {
+import Views.MainView;
+import javafx.application.Application;
+public class ApplicationLauncher{
+    public static void main(String[] args) {
+        // Launch Application
+        Application.launch(MainView.class, args);
+    }
 }
--- src/Views/MainView.java
+++ src/Views/MainView.java
@@ -1,4 +1,20 @@
 package Views;
 
-public class MainView {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btn = new Button("OK");
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        Scene scene = new Scene(root, 1024, 768);
+        stage.setTitle("Virtual Stock");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
