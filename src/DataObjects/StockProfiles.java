e4aa1a1b191cca6e086e4152ed70bdeff7f5fd39
Weixin Tan <weixin.tan@stonybrook.edu>
2020-06-16 23:03:22 -0400

    DataObjects: Implementation of StockProfile, StockProfiles, and UserProfile (#1)
    * implemented StockProfile, StockProfiles, and UserProfile
    * removeStock() returns stockProfiles regardless of stock remove successful or not

--- src/DataObjects/StockProfiles.java
+++ src/DataObjects/StockProfiles.java
@@ -1,4 +1,27 @@
 package DataObjects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StockProfiles {
+
+    private List<StockProfile> stockProfiles;
+
+    public StockProfiles(){
+        stockProfiles = new ArrayList<>();
+    }
+
+    public List<StockProfile> getStockProfiles() {return stockProfiles;}
+
+    public List<StockProfile> addStock(StockProfile stock){
+        stockProfiles.add(stock);
+        return stockProfiles;
+    }
+
+    public List<StockProfile> removeStock(StockProfile stock){
+        stockProfiles.remove(stock);
+        return stockProfiles;
+    }
+
+
 }
--- src/DataObjects/UserProfile.java
+++ src/DataObjects/UserProfile.java
@@ -1,4 +1,47 @@
 package DataObjects;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserProfile {
+
+    private double totalInvestment, stocksValue, buyingPower, profit;
+    private String username;
+    private Map<String, Integer> stocksOwned;
+
+    public UserProfile(double totalInvestment, double stocksValue, double buyingPower, double profit,
+                        String username){
+        this.totalInvestment = totalInvestment;
+        this.stocksValue = stocksValue;
+        this.buyingPower = buyingPower;
+        this.profit = profit;
+        this.username = username;
+        this.stocksOwned =  new HashMap<>();
+    }
+
+    public double getTotalInvestment() {return totalInvestment;}
+
+    public double getStocksValue() {return stocksValue;}
+
+    public double getBuyingPower() {return buyingPower;}
+
+    public double getProfit() {return profit;}
+
+    public String getUsername() {return username;}
+
+    public void setTotalInvestment(double totalInvestment) {this.totalInvestment = totalInvestment;}
+
+    public void setStocksValue(double stocksValue) {this.stocksValue = stocksValue;}
+
+    public void setBuyingPower(double buyingPower) {this.buyingPower = buyingPower;}
+
+    public void setProfit(double profit) {this.profit = profit;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    //can be use for update and add stock own by use
+    public void setStockShares(StockProfile stock, Integer numberOfShares){stocksOwned.put(stock.getStockCode(), numberOfShares);}
+
+    @SuppressWarnings("SuspiciousMethodCalls")
+    public void removeStock(StockProfile stock){stocksOwned.remove(stock);}
 }
