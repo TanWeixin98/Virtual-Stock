a6a01c02e550c10acec2956dd3466273b2863583
Erik Zhou <54786956+Erik-Z@users.noreply.github.com>
2020-06-20 18:10:53 -0400

    Views: Added Search bar to MainView (#2)
    * Implemented Base Code for JavaFX
    
    * Implemented Base Code for JavaFX
    
    * Added search bar to MainView
    
    * removed out directory
    
    * Added Side Bar and Switched to FXML Format
    
    * Started Using the JFoenix Material Design Library
    
    * DataObjects: Implementation of StockProfile, StockProfiles, and UserProfile (#1)
    
    * implemented StockProfile, StockProfiles, and UserProfile
    * removeStock() returns stockProfiles regardless of stock remove successful or not
    
    * Implemented Base Code for JavaFX
    
    Co-authored-by: Weixin Tan <weixin.tan@stonybrook.edu>

--- src/Views/MainView.java
+++ src/Views/MainView.java
@@ -1,4 +1,24 @@
 package Views;
 
-public class MainView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+    public static final double WIDTH = 1024;
+    public static final double HEIGHT = 768;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        stage.setTitle("Virtual Stock");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
+
+
