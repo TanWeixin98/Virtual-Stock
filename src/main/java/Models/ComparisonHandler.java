a92017f27fd87298ebb5b4f4251d4020995fcc18
Erik-Z <erikzhou7@gmail.com>
2020-06-22 18:29:39 -0400

    Added Basic comparators

--- src/main/java/Models/ComparisonHandler.java
+++ src/main/java/Models/ComparisonHandler.java
@@ -1,4 +1,28 @@
 package main.java.Models;
 
+import DataObjects.StockProfile;
+
+import java.util.Comparator;
+
 public class ComparisonHandler {
+    class SortByCompanyName implements Comparator<DataObjects.StockProfile>{
+        @Override
+        public int compare(StockProfile stock1, StockProfile stock2) {
+            return stock1.getCompanyName().compareTo(stock2.getCompanyName());
+        }
+    }
+
+    class SortByStockCode implements Comparator<DataObjects.StockProfile>{
+        @Override
+        public int compare(StockProfile stock1, StockProfile stock2) {
+            return stock1.getStockCode().compareTo(stock2.getStockCode());
+        }
+    }
+
+    class SSortByCurrentPrice implements Comparator<DataObjects.StockProfile>{
+        @Override
+        public int compare(StockProfile stock1, StockProfile stock2) {
+            return (int)(stock1.getCurrPrice() - stock2.getCurrPrice());
+        }
+    }
 }
