e2082a71f3cd2703a1e4a08796519395ea950afc
weixin <weixin.tan@stonybrook.edu>
2020-06-25 23:15:56 -0400

    add get and post for jsonArray and jsonObject

--- src/main/java/Models/APIEngine.java
+++ src/main/java/Models/APIEngine.java
@@ -3,6 +3,8 @@ package Models;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -28,18 +30,72 @@ public class APIEngine {
             HttpGet httpGet = new HttpGet(url + endpoint + params);
             httpGet.setHeader("Authorization", APIKey);
 
-            String reponseBody = httpClient.execute(httpGet, responseHandler);
+            String responseBody = httpClient.execute(httpGet, responseHandler);
             httpClient.close();
 
-            return new JSONArray(reponseBody);
+            return new JSONArray(responseBody);
         }catch (Exception e){
             return null;
         }
 
     }
 
-    //TODO:
-    public JSONObject GetRequestObject(){
-        return null;
+    public JSONObject GetRequestObject(String url, String endpoint, String APIKey, String params){
+        try {
+            CloseableHttpClient httpClient = HttpClients.createMinimal();
+
+            HttpGet httpGet = new HttpGet(url + endpoint + params);
+            httpGet.setHeader("Authorization", APIKey);
+
+            String responseBody = httpClient.execute(httpGet, responseHandler);
+            httpClient.close();
+
+            return new JSONObject(responseBody);
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public JSONObject PostRequestObject(String url, String endpoint, String APIKey, String params){
+        try {
+            CloseableHttpClient httpClient = HttpClients.createMinimal();
+
+            HttpPost httpPost = new HttpPost(url + endpoint);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            httpPost.setHeader("Authorization", APIKey);
+
+            StringEntity stringEntity = new StringEntity(params);
+            httpPost.setEntity(stringEntity);
+
+            String responseBody = httpClient.execute(httpPost, responseHandler);
+            httpClient.close();
+
+            return new JSONObject(responseBody);
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+
+    public JSONArray PostRequestObjects(String url, String endpoint, String APIKey, String params){
+        try {
+            CloseableHttpClient httpClient = HttpClients.createMinimal();
+
+            HttpPost httpPost = new HttpPost(url + endpoint);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            httpPost.setHeader("Authorization", APIKey);
+
+            StringEntity stringEntity = new StringEntity(params);
+            httpPost.setEntity(stringEntity);
+
+            String responseBody = httpClient.execute(httpPost, responseHandler);
+            httpClient.close();
+
+            return new JSONArray(responseBody);
+        }catch (Exception e){
+            return null;
+        }
     }
 }
