59832483bdf7b65d8278fab07eba0ac35ac776e9
weixin <weixin.tan@stonybrook.edu>
2020-06-27 20:50:01 -0400

    fix application launcher at start up

--- src/main/java/ApplicationLauncher.java
+++ src/main/java/ApplicationLauncher.java
@@ -1,9 +1,19 @@
-import main.java.Views.MainView;
+import Views.MainView;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.application.Application;
 
-public class ApplicationLauncher{
-    public static void main(String[] args) {
-        // Launch Application
-        Application.launch(MainView.class, args);
+public class ApplicationLauncher extends Application{
+
+    public void start(Stage primaryStage){
+        ViewInitialization(primaryStage);
+    }
+
+    private static void ViewInitialization(Stage primaryStage){
+        MainView mainView = new MainView();
+        Scene root = mainView.getMainView();
+        primaryStage.setScene(root, 1024, 524);
+        primaryStage.setTitle("Virtual Stock");
+        primaryStage.show();
     }
 }
