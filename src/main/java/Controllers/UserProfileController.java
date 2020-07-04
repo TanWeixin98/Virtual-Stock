97f438eee0c16ad528fa3b253a6411dd929d5060
henryli12 <xiaofengli2016@gmail.com>
2020-07-03 20:15:14 -0400

    added main controller method

--- src/main/java/Controllers/UserProfileController.java
+++ src/main/java/Controllers/UserProfileController.java
@@ -1,4 +1,18 @@
-package Controllers;
+package main.java.Controllers;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
 
 public class UserProfileController {
+    public UserProfileController() {
+
+    }
+    public static EventHandler generateHandler(Event event, String data){
+        return new EventHandler() {
+            @Override
+            public void handle(Event event) {
+
+            }
+        };
+    }
 }
