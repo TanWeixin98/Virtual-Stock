6986b390170f23103c204b992f394caa41aa3a5b
Weixin Tan <weixin.tan@stonybrook.edu>
2020-07-25 17:27:19 -0400

    V0.1 (Views): Reading View from FXML as Node (#7)
    * added a testing node to main node
    
    * View
    
    * read view from fxml and loaded in when neeeded
    * mainview sets each view during initilization stage(currently only
    userview)
    * fixed fxml version error
    * remove the right, top, bot node of the mainView

--- src/main/java/Views/MainView.java
+++ src/main/java/Views/MainView.java
@@ -1,41 +1,66 @@
 package Views;
 
+import DataObjects.UserProfile;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
 import java.net.URL;
 
 public class MainView{
 
     private static final String resourceFileName = "Views/Main.fxml";
-    private Scene mainViewScene;
+    private BorderPane mainViewNode;
 
     public MainView(){
-        OnInitialize();
+        onInitialize();
     }
 
-    private void OnInitialize(){
+    private void onInitialize(){
         try {
-            ViewInitialization();
+            loadFXMLView();
+
+            //initalize all nodes and default display is currently user profile
+            //TODO: maybe make each node into singleton in future?
+            //TODO: decide which is the default display node
+            UserProfileView userProfileView = new UserProfileView();
+            mainViewNode.setCenter(userProfileView.getUserProfileView());
         }catch (Exception e){
             //TODO: handle exception
+            System.out.println(e);
         }
         //component initialisation
         //component binding
     }
 
-
-    private void ViewInitialization() throws Exception{
+    private void loadFXMLView() throws Exception{
         URL resourceLocation = getClass().getClassLoader().getResource(resourceFileName);
         if(resourceLocation == null)
             //TODO: create new exception for ViewInitaliztion
             throw new Exception();
-        Parent mainView = FXMLLoader.load(resourceLocation);
-        mainViewScene= new Scene(mainView ,ViewConfigurations.MAIN_VIEW_WIDTH, ViewConfigurations.MAIN_VIEW_HEIGHT);
+        mainViewNode = FXMLLoader.load(resourceLocation);
+
+        //set property
+        //maybe can be done in fxml too?
+        mainViewNode.setRight(new Pane());
+        mainViewNode.getRight().maxWidth(0);
+        mainViewNode.getRight().maxHeight(0);
+
+        mainViewNode.setTop(new Pane());
+        mainViewNode.getTop().maxWidth(0);
+        mainViewNode.getTop().maxHeight(0);
+
+        mainViewNode.setBottom(new Pane());
+        mainViewNode.getBottom().maxWidth(0);
+        mainViewNode.getBottom().maxHeight(0);
     }
 
-    public Scene getMainView() {
-        return mainViewScene;
+    public Parent getMainView() {
+        return mainViewNode;
     }
 }
 
--- src/main/java/Views/SearchView.java
+++ src/main/java/Views/SearchView.java
@@ -1,6 +1,34 @@
-package main.java.Views;
+package Views;
 
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+import java.net.URL;
 
 public class SearchView{
+    private static final String resourceName = "Views/UserProfile.fxml";
+    private Node searchViewNode;
 
+    SearchView(){onInitalize(); }
+
+    public Node getUserProfileView(){
+        return searchViewNode;
+    }
+
+    private void onInitalize(){
+        try{
+            loadFXMLView();
+        }catch (Exception e){
+            //TODO: handle error
+        }
+    }
+
+    private void loadFXMLView() throws Exception {
+        URL resourceLocation = getClass().getClassLoader().getResource(resourceName);
+        if (resourceLocation == null)
+            //TODO: create new exception for ViewInitaliztion
+            throw new Exception();
+        searchViewNode = FXMLLoader.load(resourceLocation);
+    }
 }
